5daf2ddf77ade4b672b3d52ca547fc55b1eaafdb
Kirill Kulikov <58344309+Lir-kulikov@users.noreply.github.com>
2020-10-28 09:53:10 +0300

    исправляет анимацию появления меню аккаунта, добавляет в шапку копирайт в мобильной версии, исправляет в style review позиционрирование слайдера, добавляет иконку выйти к меню аккаунта

--- src/blocks/search/search.js
+++ src/blocks/search/search.js
@@ -1,15 +1,36 @@
-import {openCloseMenu} from '../../js/openCloseMenu.js';
+// import {openCloseMenu} from '../../js/openCloseMenu.js';
 
 const searchOpen = document.querySelector('.js-search-opener');
 const searchDrop = document.querySelector('.js-search-drop');
 const searchClose = document.querySelector('.js-search-close');
 const searchField = document.querySelector('.js-search-focus');
 
-openCloseMenu(searchOpen, searchDrop, searchClose);
-
 searchOpen.addEventListener('click', () => {
-  console.log('focus');
   searchField.select();
   searchField.focus();
 });
+
+const openCloseSearch = () => {
+  searchOpen.addEventListener('click', () => {
+    searchDrop.classList.toggle('is-open');
+    searchOpen.classList.toggle('is-open');
+  });
+  if (searchClose) {
+    searchClose.addEventListener('click', () => {
+      searchDrop.classList.remove('is-open');
+      searchField.value = '';
+    });
+  }
+
+  window.addEventListener('click', (e) => {
+    if (searchDrop.classList.contains('is-open') && !searchDrop.contains(e.target) && !searchOpen.contains(e.target)) {
+      searchDrop.classList.remove('is-open');
+      searchOpen.classList.remove('is-open');
+      searchField.value = '';
+    }
+})
+}
+
+openCloseSearch();
+
 
